991e63f6e0b66588a11b734284702595f5709d3c
demonoho <ceylanh666@qq.com>
2024-04-07 22:33:17 +0800

    1

--- DataStructure/zztt.cpp
+++ DataStructure/zztt.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
+const int N = 1e5 + 10;
+int n, a[10], maxn = -1;
+string s;
 int main() {
-	cout << "hello world "<<endl;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		a[s[s.size() - 1] - '0'] = max(a[s[0] - '0'] + 1, a[s[s.size() - 1] - '0']);
+	}
+	for (int i = 0; i < 10; i++) maxn = max(maxn, a[i]);
+	cout << n - maxn;
 	return 0;
 }
