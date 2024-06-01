2e2da6d5094748e50f05160b539e24f4b19b30be
demonho(pc1) <1005772973@qq.com>
2024-06-01 13:42:52 +0800

    1

---
+++ javaF/t1/src/test17/collection.java
@@ -0,0 +1,27 @@
+package test17;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class collection {
+    public static void main(String[] args) {
+        Collection<student> arr=new ArrayList<>();
+        student s1=new student(12,"asdg");
+        student s2=new student(45,"oop");
+        student s3=new student(34,"lkkj");
+        student s4=new student(23,"pou");
+        arr.add(s1);
+        arr.add(s2);
+        arr.add(s3);
+        arr.add(s4);
+        Iterator<student> it =arr.iterator();
+        while(it.hasNext()){
+            student tmp=it.next();
+            if(tmp.getAge()==34){
+                it.remove();
+            }else
+            System.out.println(tmp.toString());
+        }
+    }
+}
